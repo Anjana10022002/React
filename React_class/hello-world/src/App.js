548a8797f268a36608d3176b910a64a3d970858b
Anjana P R <pranjana333@gmail.com>
2025-12-04 22:26:00 +0530

    new react project

---
+++ React_class/hello-world/src/App.js
@@ -0,0 +1,37 @@
+// import React from 'react';
+
+// function App(){
+//   return(
+//           <div className='container'>
+//     <div className='row'>
+//       <div className='col-md-4 bg-primary mr-2'> column 1</div>
+//       <div className='col-md-4 bg-secondary' > column 2</div>
+//     </div>
+//            </div>
+//   );
+// };
+// export default App;
+
+// import "./App.css";
+// function App() {
+//   let greeting = "Hello World";
+//   console.log(greeting);
+//   return (
+//   <div>
+//   <h1 className="greetingStyle">{greeting}</h1>
+//   </div>
+//   );
+// }
+// export default App;
+
+import "./App.css";
+function App() {
+  let greeting = "Hello World";
+  return (
+  <div>
+  <h1 className="greetingStyle">{greeting}</h1>
+  <img src="https://m.media-amazon.com/images/M/MV5BYjFjZGRjMWYtZTA5MS00OTZkLWI1MjctNDQ2NmQzMWM1NzRhXkEyXkFqcGdeQXVyMjkxNzQ1NDI@._V1_.jpg"/>
+  </div>
+  );
+}
+export default App;
