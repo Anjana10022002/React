155562b58191a9e747d7d150e7abb463a097a628
Anjana P R <pranjana333@gmail.com>
2025-12-05 15:18:53 +0530

    Adding buttons

--- React_Lab_Day2_Qn-2/hwday2app/src/App.js
+++ React_Lab_Day2_Qn-2/hwday2app/src/App.js
@@ -2,9 +2,10 @@ function foodList(){
   let food = ['Pizza', 'Burger', 'Pasta', 'Salad', 'Sushi'];
   return (
     <ul>
-      {food.map((item, index) => <li key={index}>{item}</li>  )}
+      {food.map((item, index) => <li key={index}>{item}  
+        <button onClick={()=>{document.write(`I Love ${item}`)}}>Click</button>
+      </li>  )}
     </ul>
   )
-
 }
 export default foodList;
