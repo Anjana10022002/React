a036bd9e8c578ccccb85c6698ab0fb9918b87085
Anjana P R <pranjana333@gmail.com>
2025-12-07 23:30:54 +0530

    Fixing syntax error

--- React_Lab_Day1_Qn-1/cwday1app/src/App.js
+++ React_Lab_Day1_Qn-1/cwday1app/src/App.js
@@ -5,9 +5,9 @@ function welcome() {
     <div> 
       <div>
         <h2>Welcome to React Learning, {name}!</h2>
+        <img src='https://cdn-icons-png.flaticon.com/512/919/919851.png'></img>
+        <p>This is your first card with images and styles! </p>
       </div>
-      <img src='https://cdn-icons-png.flaticon.com/512/919/919851.png'></img>
-      <p>This is your first card with images and styles! </p>
     </div>
 
 
