b5cc97c7ed96a233c70f7777cde6d212bf1e849b
Anjana P R <pranjana333@gmail.com>
2025-12-07 23:30:11 +0530

    Adding description

--- React_Lab_Day1_Qn-1/cwday1app/src/App.js
+++ React_Lab_Day1_Qn-1/cwday1app/src/App.js
@@ -7,7 +7,9 @@ function welcome() {
         <h2>Welcome to React Learning, {name}!</h2>
       </div>
       <img src='https://cdn-icons-png.flaticon.com/512/919/919851.png'></img>
+      <p>This is your first card with images and styles! </p>
     </div>
+
 
   );
 }
