01078ccb054d37b8bc538ee362c8ce4536cff775
Anjana P R <pranjana333@gmail.com>
2025-12-08 23:44:40 +0530

    Adding a new message

--- React_Lab_Day3_Qn-2/hwday3app/src/App.js
+++ React_Lab_Day3_Qn-2/hwday3app/src/App.js
@@ -2,5 +2,7 @@ import React, {useState} from "react";
 
 function App() {
     const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const [message, setMessage] = useState("Add a task to ge strated!");
 
 }
