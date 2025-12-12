959b3aa4d16261047d60a88d05e07a627eecda52
Anjana P R <pranjana333@gmail.com>
2025-12-12 23:16:30 +0530

    fix: update router configuration and render App component directly

--- React_class/hello-world/src/components/router.js
+++ React_class/hello-world/src/components/router.js
@@ -5,6 +5,7 @@ import App from "./App";
 const router = createBrowserRouter([
     { path: '', element: <App/> },
     { path: 'aboutus', element: <Aboutus/> },
+    { path: '*', element: <App/> }
 ]);
 
 export default router;
