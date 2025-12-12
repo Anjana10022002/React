10a8305c790334eb4cfb54073f3fe06194cc8aef
Anjana P R <pranjana333@gmail.com>
2025-12-12 15:08:09 +0530

    feat: add scheduler package and set-cookie-parser dependency
    - Added scheduler package (version 0.27.0) with production and development builds.
    - Introduced set-cookie-parser package (version 2.7.2) for parsing set-cookie headers.
    - Updated package.json to include new dependencies and their respective licenses.
    - Created necessary files for both packages including README, LICENSE, and implementation files.

--- React_class/hello-world/src/App.js
+++ React_class/hello-world/src/App.js
@@ -24,14 +24,26 @@
 // }
 // export default App;
 
-import "./App.css";
+// import "./App.css";
+// function App() {
+//   let greeting = "Hello World";
+//   return (
+//   <div>
+//   <h1 className="greetingStyle">{greeting}</h1>
+//   <img src="https://m.media-amazon.com/images/M/MV5BYjFjZGRjMWYtZTA5MS00OTZkLWI1MjctNDQ2NmQzMWM1NzRhXkEyXkFqcGdeQXVyMjkxNzQ1NDI@._V1_.jpg"/>
+//   </div>
+//   );
+// }
+// export default App;
+
+import React from 'react';
+
 function App() {
-  let greeting = "Hello World";
-  return (
+  return(
   <div>
-  <h1 className="greetingStyle">{greeting}</h1>
-  <img src="https://m.media-amazon.com/images/M/MV5BYjFjZGRjMWYtZTA5MS00OTZkLWI1MjctNDQ2NmQzMWM1NzRhXkEyXkFqcGdeQXVyMjkxNzQ1NDI@._V1_.jpg"/>
+    <p>Home component </p>
   </div>
-  );
+);
 }
+
 export default App;
