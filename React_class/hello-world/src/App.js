5a5a34ec25559e9ff7e410def4362ad975690a7a
Anjana P R <pranjana333@gmail.com>
2025-12-12 15:20:20 +0530

    Fixing syntax error

--- React_class/hello-world/src/App.js
+++ React_class/hello-world/src/App.js
@@ -36,11 +36,25 @@
 // }
 // export default App;
 
-import React from 'react';
+// import React from 'react';
+
+// function App() {
+//   return(
+//   <div>
+//     <p>Home component </p>
+//   </div>
+// );
+// }
+
+// export default App;
+
+import React , { useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return(
   <div>
+    <Navbar/>
     <p>Home component </p>
   </div>
 );
