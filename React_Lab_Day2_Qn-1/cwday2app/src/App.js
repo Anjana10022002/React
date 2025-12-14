7ad346af9b575cbf7011280cbe8ceaf0c7c41a22
Anjana P R <pranjana333@gmail.com>
2025-12-14 22:50:32 +0530

    Adding hobbie list

--- React_Lab_Day2_Qn-1/cwday2app/src/App.js
+++ React_Lab_Day2_Qn-1/cwday2app/src/App.js
@@ -2,12 +2,27 @@ function PersonalInfo(){
   const name = "Alex";
   const age = 20;
   const isStudent = true;
+  const hobbies = ["Reading", "Hiking", "Coding"];
+  const hobbieList = [ ];
+  for( let i= 0; i < hobbies.length; i++){
+    hobbieList.push(<li>{hobbies[i]}</li>)
+  }
   return(
+    <>
     <div className="container text-center bg-light m-auto p-5 mx-auto w-50 border border-dark mt-5">
       <p>Name : {name}</p>
       <p>Age : {age}</p>
       <p>Are you a student : {isStudent.toString()}</p>
     </div>
+    <div>
+      <p>Displaying hobby list using for loop</p>
+      <ul>{hobbieList}</ul>
+
+      <p>Displaying hobby list using map function</p>
+      {hobbies.map((item)=>{return <ul><li>{item}</li></ul>})}
+    </div>
+    </>
+    
   )
 }
 export default PersonalInfo;
