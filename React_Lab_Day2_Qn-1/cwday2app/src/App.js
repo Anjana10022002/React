cde5aee4669710b8f4edba838f07ef510f31df72
Anjana P R <pranjana333@gmail.com>
2025-12-14 22:34:46 +0530

    feat: replace default App component with PersonalInfo component displaying user details

--- React_Lab_Day2_Qn-1/cwday2app/src/App.js
+++ React_Lab_Day2_Qn-1/cwday2app/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function PersonalInfo(){
+  const name = "Alex";
+  const age = 20;
+  const isStudent = true;
+  return(
+    <div className="container text-center bg-light m-auto p-5 mx-auto w-50 border border-dark mt-5">
+      <p>Name : {name}</p>
+      <p>Age : {age}</p>
+      <p>Are you a student : {isStudent.toString()}</p>
     </div>
-  );
+  )
 }
-
-export default App;
+export default PersonalInfo;
