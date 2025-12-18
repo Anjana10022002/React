96311e868cbf59472a0f10a31781bab16bce65fe
Anjana P R <pranjana333@gmail.com>
2025-12-18 23:12:44 +0530

    refactor: clean up App component by removing unused imports and structure

--- React_Lab_Day8_Qn-2/hwday8app/src/App.jsx
+++ React_Lab_Day8_Qn-2/hwday8app/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
 
 function App() {
- 
-
   return (
-    <>
-      <h1></h1>
-    </>
-  )
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
-export default App
+export default App;
