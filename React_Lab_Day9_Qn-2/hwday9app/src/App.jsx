ea51ec7f4d5637bd9ce0bd191d6b2a2154dbc96b
Anjana P R <pranjana333@gmail.com>
2025-12-19 23:32:56 +0530

    feat: add routing and components for Home, About, and Contact

--- React_Lab_Day9_Qn-2/hwday9app/src/App.jsx
+++ React_Lab_Day9_Qn-2/hwday9app/src/App.jsx
@@ -1,1 +1,6 @@
-from "./components/Counter";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact"; 
