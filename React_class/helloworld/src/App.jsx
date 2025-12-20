5f7fd911f4c5f9a626060b44554e77e5fe239406
Anjana P R <pranjana333@gmail.com>
2025-12-20 23:23:20 +0530

    Printing variable in console

--- React_class/helloworld/src/App.jsx
+++ React_class/helloworld/src/App.jsx
@@ -55,18 +55,30 @@
 //     </div>
 //   );
 // }
+// // export default App;
+
+// import React from 'react';
+
+// function App(){
+//   return(
+//           <div className='container'>
+//     <div className='row'>
+//       <div className='col-md-4 bg-primary mr-2'> column 1</div>
+//       <div className='col-md-4 bg-secondary' > column 2</div>
+//     </div>
+//            </div>
+//   );
+// };
 // export default App;
 
-import React from 'react';
-
-function App(){
-  return(
-          <div className='container'>
-    <div className='row'>
-      <div className='col-md-4 bg-primary mr-2'> column 1</div>
-      <div className='col-md-4 bg-secondary' > column 2</div>
-    </div>
-           </div>
+import "./App.css";
+function App() {
+  let greeting = "Hello World";
+  console.log(greeting);
+  return (
+  <div>
+  <h1 className="greetingStyle">{greeting}</h1>
+  </div>
   );
-};
+}
 export default App;
