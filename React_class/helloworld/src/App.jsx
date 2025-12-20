295023313b805ba5ccf1983658132dfd45c75a28
Anjana P R <pranjana333@gmail.com>
2025-12-20 23:19:18 +0530

    Writing Hello World with external CSS

--- React_class/helloworld/src/App.jsx
+++ React_class/helloworld/src/App.jsx
@@ -27,10 +27,21 @@
 // }
 // export default App;
 
+// function App() {
+//   return (
+//     <div>
+//     <h1 style={{color:"red"}}>Hello World</h1>
+//     </div>
+//   );
+// }
+// export default App;
+
+import "./App.css";
 function App() {
+  let greeting = "Hello World";
   return (
     <div>
-    <h1 style={{color:"red"}}>Hello World</h1>
+    <h1 className="greetingStyle">{greeting}</h1>
     </div>
   );
 }
