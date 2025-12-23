d1a7d7154f9e35f5a426fd074c9bb5a619838e96
Anjana P R <pranjana333@gmail.com>
2025-12-23 21:18:19 +0530

    Passing function reference through props

--- React_class/props/src/App.jsx
+++ React_class/props/src/App.jsx
@@ -11,17 +11,35 @@
 
 // export default App;
 
-import React from 'react';
+// import React from 'react';
+// import Child from './child';
+
+// function App() {
+//   const name = "Alice";
+//   const age = 25;
+//   return ( 
+//     <Child name={name} age={age} >
+//        <p> Welcome to mashup stack </p>
+//    </Child>
+//    );
+// }
+
+// export default App;
+
+import React, {useState} from 'react';
 import Child from './child';
 
 function App() {
-  const name = "Alice";
-  const age = 25;
-  return ( 
-    <Child name={name} age={age} >
-       <p> Welcome to mashup stack </p>
-   </Child>
-   );
+   const [count, setCount] = useState(0);
+   function incrementCount(){
+  setCount(count + 1);
+    }
+   return (
+       <div>
+            <h1>Count: { count } </h1>
+            <Child increment={incrementCount} />
+      </div>
+  );
 }
-
+  
 export default App;
--- React_class/props/src/child.jsx
+++ React_class/props/src/child.jsx
@@ -12,15 +12,24 @@
 
 
 
+// import React from 'react';
+
+// function Child(props) {
+//        return ( 
+//             <div>
+//                 <p> Name : {props.name} </p>
+//                 <p>Age : {props.age} </p>
+//                 <p>{props.children}</p>
+//            </div>
+//        );
+// }
+// export default Child;
+
 import React from 'react';
 
-function Child(props) {
-       return ( 
-            <div>
-                <p> Name : {props.name} </p>
-                <p>Age : {props.age} </p>
-                <p>{props.children}</p>
-           </div>
-       );
+function Child(props){
+    return( 
+        <button onClick={props.increment}> Increment count </button>
+    )
 }
 export default Child;
