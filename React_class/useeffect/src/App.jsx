5544d34efe10cc7cfe24382250eb6742534d2539
Anjana P R <pranjana333@gmail.com>
2025-12-24 23:42:44 +0530

    Do something when the component is re-rendered

--- React_class/useeffect/src/App.jsx
+++ React_class/useeffect/src/App.jsx
@@ -1,10 +1,32 @@
+// import { useState, useEffect } from 'react';
+
+// function App(){
+//   const [count, setCount] = useState(0);
+//   useEffect( () =>{
+//     console.log("component has rendered");
+//   }, [ ]);
+  
+//   const handleClick = () => {
+//     setCount(count + 1);
+//   };
+
+//     return (
+//   <div>
+//   <p>Count : {count}</p>
+//   <button onClick={handleClick} > Increment count </button>
+//   </div>
+// );
+// }
+// export default App;
+
+
 import { useState, useEffect } from 'react';
 
 function App(){
   const [count, setCount] = useState(0);
   useEffect( () =>{
     console.log("component has rendered");
-  }, [ ]);
+  });
   
   const handleClick = () => {
     setCount(count + 1);
