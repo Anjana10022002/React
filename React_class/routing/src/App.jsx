da599152d54e032f25f7202d4a37ccfde06b0baa
Anjana P R <pranjana333@gmail.com>
2025-12-25 23:31:05 +0530

    Updaing app.jsx

--- React_class/routing/src/App.jsx
+++ React_class/routing/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+// import React from 'react';
+
+// function App() {
+//   return(
+//   <div>
+//     <p>Home component </p>
+//   </div>
+// );
+// }
+
+// export default App;
+
+import React , { useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return(
   <div>
+    <Navbar/>
     <p>Home component </p>
   </div>
 );
