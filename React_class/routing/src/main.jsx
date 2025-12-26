06f20efba802473cfa6a9554666d9df26af29c2f
Anjana P R <pranjana333@gmail.com>
2025-12-26 23:21:20 +0530

    Remove unused import of reportWebVitals in main.jsx

--- React_class/routing/src/main.jsx
+++ React_class/routing/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,3 +11,4 @@ root.render(
 );
 
 reportWebVitals();
+
