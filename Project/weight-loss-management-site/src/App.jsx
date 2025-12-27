344d83d4a29f3711fee8e133a8ad69e8a6237afa
Anjana P R <pranjana333@gmail.com>
2025-12-27 23:48:01 +0530

    Creating app funtion

--- Project/weight-loss-management-site/src/App.jsx
+++ Project/weight-loss-management-site/src/App.jsx
@@ -1,1 +1,18 @@
-import {}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
