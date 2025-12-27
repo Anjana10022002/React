93724e678196202a19ca87340b0f1c2b2e9b73f3
Anjana P R <pranjana333@gmail.com>
2025-12-27 23:51:20 +0530

    Adding register function input feilds

--- Project/weight-loss-management-site/src/pages/Signup.jsx
+++ Project/weight-loss-management-site/src/pages/Signup.jsx
@@ -2,3 +2,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Signup() {
+    var {name, setName} = useState("");
+    var {email, setEmail} = useState("");
+    var {password, setPassword} = useState("");
+    var {passwordConfirm, setPasswordConfirm} = useState("");
+    var navigate = useNavigate();
