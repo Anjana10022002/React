58733b2ec0fea2f55d7ebed4bd9bae01b52ce2f3
Anjana P R <pranjana333@gmail.com>
2025-12-27 23:57:02 +0530

    Created sign up page

--- Project/weight-loss-management-site/src/pages/Signup.jsx
+++ Project/weight-loss-management-site/src/pages/Signup.jsx
@@ -14,4 +14,64 @@ function Signup() {
             password: password,
             passwordConfirm: passwordConfirm
         }
+        axios.post("http://localhost:3000/api/v1/users/signup", user)
+        .then(response => {
+            setErrorMessage("");
+            navigate("/");
+        }) .catch(error=>{
+            if(error.response.data.errors){
+                setErrorMessage(Object.values(error.response.data.errors).join(' '));
+            }else{
+                setErrorMessage('Failed to connect to api');
+            }
+        })
     }
+    return <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col-8 offset-2">
+                    <h1>Register</h1>
+                    {errorMessage?<div className="alert alert-danger">{errorMessage}</div>:''}
+                    <div className="form-group">
+                        <label>Name:</label>
+                        <input type="text"
+                        className="form-control"
+                        value={name}
+                        onInput={(event)=>setName(event.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input type="text"
+                        className="form-control"
+                        value={email}
+                        onInput={(event)=>setEmail(event.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password:</label>
+                        <input type="password"
+                        className="form-control"
+                        value={password}
+                        onInput={(event)=>setPassword(event.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Confirm Password:</label>
+                        <input type="password"
+                        className="form-control"
+                        value={passwordConf}
+                        onInput={(event)=>setPasswordConf(event.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary float-right" onClick={registerUser}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Signup;
